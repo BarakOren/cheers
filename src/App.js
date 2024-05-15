b74b0dcdcfa5fc3962e8e41c0a0e7b2b1df940bc
BarakOren <shaharoren1234@gmail.com>
2024-05-15 15:55:23 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { info } from './info';
+import { useState } from 'react';
 
 function App() {
+
+  const [current, setCurrent] = useState({name: "hello" , type: "hello"})
+
+  const Shuffle = () => {
+    const num = Math.floor( Math.random() * (61 - 0) + 0)
+    console.log(num)
+    const random = info[num] || {name: "err" , type: "err"}
+    setCurrent(random)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1 style={{fontSize: '52px'}}>Mixer</h1>
+        
+        <div className="content">
+        <h1 class="name">{current.name}</h1>
+        <h1 class="type">{current.type}</h1>
+        <button onClick={() => Shuffle()}>Shuffle</button>
+        </div>
     </div>
   );
 }
---
+++ src/info.js
@@ -0,0 +1,235 @@
+export const info = [
+    {
+        name: 'ignore',
+        type: 'ignore'
+    },
+    {
+        name: 'Molinari',
+        type: 'liqueur '
+    },
+    {
+        name: 'Artesano',
+        type: 'RUM'
+    },
+    {
+        name: 'Havana club',
+        type: 'RUM'
+    },
+    {
+        name: 'Mezcal la escondida',
+        type: 'Tequila'
+    },
+    {
+        name: 'Donjulio',
+        type: 'Tequila'
+    },
+    {
+        name: 'Petron',
+        type: 'Tequila'
+    },
+    {
+        name: '1800 anejo',
+        type: 'Tequila'
+    },
+    {
+        name: 'Southern comfort',
+        type: 'liqueur'
+    },
+    {
+        name: 'Drambuie',
+        type: 'honey liquer'
+    },
+    {
+        name: 'Dom Benedictine',
+        type: 'caramel liker'
+    },
+    {
+        name: 'Kaluha',
+        type: 'coffee liker'
+    },
+    {
+        name: 'Disaronno',
+        type: 'karamel liker'
+    },
+    {
+        name: 'Midori',
+        type: 'melon liker crystal bottel'
+    },
+    {
+        name: 'Wenneker',
+        type: 'none alcoholic liker'
+    },
+    {
+        name: 'La belle orange',
+        type: 'Orange Liqueur'
+    },
+    {
+        name: 'Malibo',
+        type: 'white rum'
+    },
+    {
+        name: 'Remy marten xo',
+        type: 'koniaq'
+    },
+    {
+        name: 'Hennessy vsop',
+        type: 'koniaq'
+    },
+    {
+        name: 'Hennessy vs',
+        type: 'koniaq'
+    },
+    {
+        name: 'Dimple',
+        type: 'SCOTCH WHISKY'
+    },
+    {
+        name: 'Dewars 12',
+        type: 'SCOTCH WHISKY'
+    },
+    {
+        name: 'Ballentines',
+        type: 'SCOTCH WHISKY'
+    },
+    {
+        name: 'Teeling',
+        type: 'Irish whiskey'
+    },
+    {
+        name: 'Redbreast single pot still',
+        type: 'Irish whiskey'
+    },
+    {
+        name: 'Jamson black barrel',
+        type: 'Irish whiskey'
+    },
+    {
+        name: 'Three olives',
+        type: 'vodka'
+    },
+    {
+        name: 'Monkey 47',
+        type: 'gin'
+    },
+    {
+        name: 'Tanqueray ten',
+        type: 'gin'
+    },
+    {
+        name: 'Tanqueray',
+        type: 'gin'
+    },
+    {
+        name: 'The balvenie',
+        type: 'scotch whisky'
+    },
+    {
+        name: 'Glenmorangie',
+        type: 'Scotch whiskey'
+    },{
+        name: 'The glenlivet 15',
+        type: 'Scotch whiskey'
+    },{
+        name: 'The glenlivet founders reserve',
+        type: 'Scotch whiskey'
+    },{
+        name: 'Glenfiddich 12 15 18',
+        type: 'Scotch whiskey'
+    },{
+        name: 'Bowmore 12',
+        type: 'Scotch Whiskey'
+    },{
+        name: 'Bowmore 15',
+        type: 'Scotch Whiskey'
+    },{
+        name: 'Bruichladdich',
+        type: 'Scotch Whiskey'
+    },{
+        name: 'Port charlotte',
+        type: 'Scotch Whiskey'
+    },{
+        name: 'Laphroaig 10',
+        type: 'Scotch Whiskey'
+    },{
+        name: 'Compass box the peqt monster',
+        type: 'Scotch Whiskey'
+    },{
+        name: 'Remy marten vsop',
+        type: 'whiskey'
+    },
+    ,{
+        name: 'Martini',
+        type: 'gin'
+    },    ,{
+        name: 'Barbero vermoth',
+        type: 'vermoth'
+    },    ,{
+        name: 'Jim beam black',
+        type: 'American bourbon'
+    },    ,{
+        name: 'Jim beam rye',
+        type: 'American bourbon'
+    },    ,{
+        name: 'Knob creek',
+        type: 'bourbon'
+    },    ,{
+        name: 'Gentlemen jack',
+        type: 'bourbon'
+    },    ,{
+        name: 'Jack daniels signel barrel',
+        type: 'bourbon'
+    },    ,{
+        name: 'Glen scotia',
+        type: 'scoti'
+    },    ,{
+        name: 'Macallan 12',
+        type: 'scoti'
+    },    ,{
+        name: 'Tomatin',
+        type: 'scoti'
+    },
+
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
